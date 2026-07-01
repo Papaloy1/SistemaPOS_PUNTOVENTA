41137abf32264791a2ed60c6c164616a66730901
Bryan Hernandez <bryanbhdzp24@gmail.com>
2026-07-01 15:33:26 -0700

    fix: corregir conexion a base de datos - cambiar servidor de PCMaster a (local)

--- SistemaPOS.GVG.API/Migrations/20260701223259_MigracionCompleta.Designer.cs
+++ SistemaPOS.GVG.API/Migrations/20260701223259_MigracionCompleta.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaPOS.API.Data;
 
@@ -10,9 +11,11 @@ using SistemaPOS.API.Data;
 namespace SistemaPOS.GVG.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260701223259_MigracionCompleta")]
+    partial class MigracionCompleta
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
